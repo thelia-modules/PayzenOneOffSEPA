14617d94fd4d19175864d05666e4495071f38a09
Soldras <antony.penalver@gmail.com>
2015-10-11 15:02:25 +0200

    Change mail templating

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,4 +1,15 @@
 <?php
 return array(
-    // 'an english string' => 'La traduction française de la chaine',
+    "Dear customer," => "Bonjour,",
+    "Your SEPA payment information are well registered for your order" => "Confirmation du paiement de votre commande",
+    "via Payzen on our shop." => "via Payzen.",
+    "Reference %ref" => "N&deg; %ref",
+    "You'll receive another email after the payment is made." => "Vous recevrez un email après que le prélèvement soit effectué.",
+    "Your order is visible in your customer account on" => "Le suivi de votre commande est disponible dans la rubrique 'Mon compte' sur",
+    "Thank you for your order !" => "Merci pour votre achat !",
+    "The %name team." => "L'équipe %name",
+    "The SEPA payment is confirmed for your order" => "Confirmation du paiement de votre commande",
+    "Your invoice is now available in your customer account on" => "Votre facture est maintenant disponible dans la rubrique 'Mon compte' sur",
+    "Payment confirmation" => "Confirmation de paiement",
+    "Order confirmation" => "Confirmation de commande",
 );
